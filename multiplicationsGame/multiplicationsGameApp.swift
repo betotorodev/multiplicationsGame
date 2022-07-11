67a16cd704d752f3a2df5c77a9f3b569878729c4
Beto Toro <beto.toro@zemoga.com>
2022-07-11 12:58:24 -0500

    Initial Commit

---
+++ multiplicationsGame/multiplicationsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiplicationsGameApp.swift
+//  multiplicationsGame
+//
+//  Created by Beto Toro on 11/07/22.
+//
+
+import SwiftUI
+
+@main
+struct multiplicationsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
